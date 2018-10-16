d7d0ae6b5a2166e0d1f2aa47c04324dd50b4c6f8
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-16 21:47:06 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,14 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+    end
+    body = JSON.parse(@resp.body)
+    if @resp.success?
+      @venues = body["response"]["venues"]
+    else
+      @error = body["meta"]["errorDetail"]
+    end
+    render 'search'
   end
 end
